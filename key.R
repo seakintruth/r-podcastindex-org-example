e252d18b98799a5622d0d65b7f79ed432cf5ce9e
Jeremy D. Gerdes <seakintruth@gmail.com>
2021-04-12 19:06:41 -0400

    Update key.R

--- key.R
+++ key.R
@@ -1,2 +1,2 @@
-Sys.setenv(podcast.index.api.key = "{ENTER YOUR KEY HERE}")
-Sys.setenv(podcast.index.api.secret = "{ENTER YOUR SECRET HERE}")
+Sys.setenv(podcast.index.api.key = "ENTER YOUR KEY HERE")
+Sys.setenv(podcast.index.api.secret = "ENTER YOUR SECRET HERE")
