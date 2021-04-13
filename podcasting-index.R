b5cace55aa7531282abbd35ac00b341cb1494aef
Jeremy D. Gerdes <seakintruth@gmail.com>
2021-04-12 20:51:12 -0400

    Add files via upload

--- podcasting-index.R
+++ podcasting-index.R
@@ -1,7 +1,19 @@
 #!/usr/bin/env Rscript
+# Example command line call from the terminal
+# --------------------------------------------
+# $ ./podcasting-index.R 'no agenda' ./key.R
+
 # Dependancies:
 # This script was developed on linux/ubuntu and depends on having previously installed these packages: 
 # sudo apt install libssl-dev openssl curl libcurl4-openssl-dev 
+
+# Intial working R Example (a single function as a begining for a package)
+# this R script was crafted after the python example see:
+# https://github.com/tbowers/python-podcastindex-org-example/blob/master/podcasting-index.py
+# and guidance from this vignette
+# https://cran.r-project.org/web/packages/httr/vignettes/api-packages.html
+# setup some basic vars for the search api. 
+# for more information, see https://api.podcastindex.org/developer_docs
 
 # If missing then install pacman
 if (!require("pacman")) install.packages("pacman")
@@ -11,39 +23,34 @@ pacman::p_load(
 	jsonlite,
         digest,
 	askpass,
-	argparser
+	argparse
 )
 
-# Intial working R Example (a single function as a begining for a package)
-# this R script was crafted after the python example see:
-# https://github.com/tbowers/python-podcastindex-org-example/blob/master/podcasting-index.py
-# and guidance from this vignette
-# https://cran.r-project.org/web/packages/httr/vignettes/api-packages.html
-# setup some basic vars for the search api. 
-# for more information, see https://api.podcastindex.org/developer_docs
-podcast_index_api_search_byterm <- function(...){
+# Set key and secret
+if(interactive()) { 
+	api_key <- askpass::askpass(
+		prompt = 'ENTER YOUR PODCAST INDEX API KEY HERE: '
+	)
+	api_secret <- askpass::askpass(
+		prompt = 'ENTER YOUR PODCAST API SECRET HERE: '
+	)
+	search_terms <- readline(prompt="Enter search terms:\n")
+} else {
+	# Handle cli Arguments if they exist
+	args <- commandArgs(trailingOnly=TRUE)
+	search_terms <- args[1]
+	KeySecretFilePath <- args[2]
+	if(file.exists(KeySecretFilePath)){
+		source(KeySecretFilePath)
+	} else {
+		message("Usage: \n ./podcasting-index.r 'search terms' /path/to/secret/key.r ")
+	}
+}
+
+# The search function
+podcast_index_api_search_byterm <- function(...,api_key,api_secret){
    search_terms <- list(...)
    if(length(search_terms)>0){
-	# is the key set to environment variable?
-	if ("" == Sys.getenv("podcast.index.api.key")){ 
-		Sys.setenv(
-			podcast.index.api.key = askpass::askpass(
-				prompt = 'ENTER YOUR PODCAST INDEX API KEY HERE: '
-			)
-		)
-	}
-	api_key <- Sys.getenv("podcast.index.api.key")
-
-	# is the key secret set to environment variable?
-	if ("" == Sys.getenv("podcast.index.api.secret")){ 
-		Sys.setenv(
-			podcast.index.api.secret = askpass::askpass(
-				prompt = 'ENTER YOUR PODCAST API SECRET HERE: '
-			)
-		)
-	}
-	api_secret <- Sys.getenv("podcast.index.api.secret")
-
 	search_queries <- URLencode(paste0("q=",paste(search_terms,sep="",collapse=" ")))
 	#q=[search terms
 	urlTarget <- paste0("https://api.podcastindex.org/api/1.0/search/byterm")
@@ -57,8 +64,7 @@ podcast_index_api_search_byterm <- function(...){
 			Sys.info()[c("machine","sysname","release")],
 			sep = "",
 			collapse = " "
-		),
-		"; script; podcasting-index-r-cli)"
+		),")"
 	)	
 	# For fun: Sets User-Agent to something like (that date is the R version date):
 	# R version 4.0.5 (2021-03-31) (x86_64 Linux 5.8.0-48-generic; script; podcasting-index-r-cli)
@@ -85,22 +91,11 @@ podcast_index_api_search_byterm <- function(...){
 			Authorization = sha_1,
 			Accept = "application/json"
 		)
+	
 	)
 
-# NOT RUN, needs further testing:  
-#	if (http_error(response)) {
-#		stop(
-#		      sprintf(
-#		        "GitHub API request failed [%s]\n%s\n<%s>", 
-#		        status_code(response),
-#		        parsed$message,
-#		        parsed$documentation_url
-#		      ),
-#		      call. = FALSE
-#		)
-#		return(NULL)
-#	}
-
+	# Comment this line out for production, displays results
+	message(jsonlite::prettify(content(response, "text",indent=4)))
 	parsed <- jsonlite::fromJSON(content(response, "text"), simplifyVector = FALSE)
 	return(parsed)
    }else{
@@ -108,6 +103,12 @@ podcast_index_api_search_byterm <- function(...){
    }
 }
 
-check <-podcast_index_api_search_byterm("no","agenda","wayback") 
 
-message(check)
+# Call the search function
+search_results <- podcast_index_api_search_byterm(
+	search_terms,
+	api_key=api_key,
+	api_secret=api_secret
+)
+# now use the values stored in (search_results) 
+
